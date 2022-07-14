e57bb97ad73896754c6fe07c40ab1548c2aa9c27
TrvMarco <trevisanello.marco@gmail.com>
2022-07-14 21:16:35 +0200

    Aggiunto tabella rotta comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,13 +6,33 @@
 
 @section('page-content')
     <div>
-        <strong>Index</strong>
-        <div class="container">
-            <ul>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Immagine</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data sconto</th>
+                <th scope="col">Tipologia</th>
+              </tr>
+            </thead>
+            <tbody>
                 @foreach ($comics as $comic)
-                    <li>Titolo: {{$comic->title}}</li>
+                <tr>
+                    <th scope="row">{{$comic->id}}</th>
+                    <td class="img-fluid"><img style="height:150px;" src="{{$comic->thumb}}" alt=""></td>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->description}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                </tr>
                 @endforeach
-            </ul>
-        </div>
+            </tbody>
+          </table>
     </div>
 @endsection
